4fec9d6af468551e86f5b438217920bfed70b4fa
Anantha Krishnan <ananthakrishnan.s@scrapehero.com>
2025-09-29 22:38:01 +0530

    feat(two-sum): add solution

---
+++ problems/easy/two-sum/py/solution_v1.py
@@ -0,0 +1,7 @@
+def twoSum(nums, target):
+    index_map = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in index_map:
+            return [index_map[complement], i]
+        index_map[num] = i
